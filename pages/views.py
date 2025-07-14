de2d6cbf2440d365f794797a02e22a1dee9446a9
Mohamed Zidane <mohamedzidane11@gmail.com>
2025-07-14 21:19:44 +0300

    creating index.html, include navbar-footer to base.html, render the index.html view func

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1 style='background-color:powderblue';>Hello world</h1>")
+    return render(request, 'pages/index.html')
